e8f6372002756a3984299d7fcdfeb4fe202f4893
multifrukt <multifrukt@users.noreply.github.com>
2023-11-23 20:19:06 -0500

    apiserver

---
+++ apiserver/apiserver.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+@app.route('/', methods=['POST'])
+def process_request():
+    data = request.json
+    name = data.get('name', 'Unknown')
+    profession = data.get('profession', 'profession')
+    response = {"message": f"{name} is a cool {profession}"}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7001)
